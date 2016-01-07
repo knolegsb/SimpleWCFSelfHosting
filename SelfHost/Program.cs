43cb9d1fc3e39284320944405e8d5800d364f6e3
knolegsb <seon2you@hotmail.com>
2016-01-07 00:37:27 -0800

    3 clients

--- SelfHost/Program.cs
+++ SelfHost/Program.cs
@@ -13,30 +13,72 @@ namespace SelfHost
     {
         static void Main(string[] args)
         {
-            using (var servicehost = new ServiceHost(typeof(MathService)))
+            using (var servicehost = new ServiceHost(
+           typeof(MathService)))
             {
-                ServiceEndpoint BasicHttpEndPoint1 = servicehost.AddServiceEndpoint(typeof(IMathService), new BasicHttpBinding(), "Http://localhost:4444/MathService");
 
-                ServiceEndpoint BasicHttpEndPoint2 = servicehost.AddServiceEndpoint(typeof(IMathService), new BasicHttpBinding(), "Http://localhost:5555/MathService");
+                ServiceEndpoint BasicHttpEndPoint1 = servicehost.AddServiceEndpoint(
+                  typeof(IMathService),
+                  new BasicHttpBinding(),
+                  "http://localhost:4444/MathService");
 
-                ServiceEndpoint NettcpendPoint = servicehost.AddServiceEndpoint(typeof(IMathService), new NetTcpBinding(), "net.tcp://localhost:6666/IMathService");
+                ServiceEndpoint BasicHttpEndPoint2 = servicehost.AddServiceEndpoint(
+                 typeof(IMathService),
+                 new BasicHttpBinding(),
+                 "http://localhost:5555/MathService");
+
+
+                ServiceEndpoint NettcpEndPoint = servicehost.AddServiceEndpoint(
+                  typeof(IMathService),
+                  new NetTcpBinding(),
+                  "net.tcp://localhost:6666/MathService");
+
 
                 servicehost.Open();
 
                 Console.WriteLine("Your WCF service is running.");
-                Console.WriteLine("Your WCF Math service is running and is listening on below endpoints:");
-                Console.WriteLine("{0} ({1})", BasicHttpEndPoint1.Address.ToString(), BasicHttpEndPoint1.Binding.Name);
+                Console.WriteLine(
+                  "Your WCF Math service is running and is listening on below endpoints:");
+                Console.WriteLine("{0} ({1})",
+                  BasicHttpEndPoint1.Address.ToString(), BasicHttpEndPoint1.Binding.Name);
 
-                foreach(ServiceEndpoint endpoint in servicehost.Description.Endpoints)
+
+                foreach (ServiceEndpoint endpoint in servicehost.Description.Endpoints)
                 {
-                    Console.WriteLine("Address : {0} Binding Name : {1}", endpoint.Address.ToString(), endpoint.Binding.Name);
+                    Console.WriteLine("Address : {0} Binding Name : {1}",
+                      endpoint.Address.ToString(), endpoint.Binding.Name);
                 }
                 Console.WriteLine();
                 Console.WriteLine("Press any key to stop your WCF Math service.");
                 Console.ReadKey();
 
+
                 servicehost.Close();
             }
+            //using (var servicehost = new ServiceHost(typeof(MathService)))
+            //{
+            //    ServiceEndpoint BasicHttpEndPoint1 = servicehost.AddServiceEndpoint(typeof(IMathService), new BasicHttpBinding(), "Http://localhost:4444/MathService");
+
+            //    ServiceEndpoint BasicHttpEndPoint2 = servicehost.AddServiceEndpoint(typeof(IMathService), new BasicHttpBinding(), "Http://localhost:5555/MathService");
+
+            //    ServiceEndpoint NettcpendPoint = servicehost.AddServiceEndpoint(typeof(IMathService), new NetTcpBinding(), "net.tcp://localhost:6666/MathService");
+
+            //    servicehost.Open();
+
+            //    Console.WriteLine("Your WCF service is running.");
+            //    Console.WriteLine("Your WCF Math service is running and is listening on below endpoints:");
+            //    Console.WriteLine("{0} ({1})", BasicHttpEndPoint1.Address.ToString(), BasicHttpEndPoint1.Binding.Name);
+
+            //    foreach(ServiceEndpoint endpoint in servicehost.Description.Endpoints)
+            //    {
+            //        Console.WriteLine("Address : {0} Binding Name : {1}", endpoint.Address.ToString(), endpoint.Binding.Name);
+            //    }
+            //    Console.WriteLine();
+            //    Console.WriteLine("Press any key to stop your WCF Math service.");
+            //    Console.ReadKey();
+
+            //    servicehost.Close();
+        
         }
     }
 }
